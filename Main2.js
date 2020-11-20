627810cb21423d984d2f3ebe2e0d5d857543a519
coder-27 <73887310+coder-27@users.noreply.github.com>
2020-11-20 12:59:34 +0530

    Create Main2.js

---
+++ Main2.js
@@ -0,0 +1,10 @@
+player1_name = localStorage.getItem("Player1_Name");
+player2_name = localStorage.getItem("Player2_Name");
+player1_score = 0;
+player2_score = 0;
+document.getElementById("player1_name").innerHTML = player1_name + " : ";
+document.getElementById("player2_name").innerHTML = player2_name + " : ";
+document.getElementById("player1_score").innerHTML = " " + player1_score;
+document.getElementById("player2_score").innerHTML = " " + player2_score;
+document.getElementById("player_quest").innerHTML = "question Turn - " + player1_name;
+document.getElementById("player_ans").innerHTML = "Answer Turn - " + player2_name;
